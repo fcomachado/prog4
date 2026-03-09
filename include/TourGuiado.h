29b13543eb18c097b101da452645564d3d213b08
p1p174 <pianavattaqu@gmail.com>
2026-03-09 19:54:42 -0300

    agregué clase TourGuiado y dejo creado TourGuiado.cpp

---
+++ include/TourGuiado.h
@@ -0,0 +1,27 @@
+#include <set>
+#include <string>
+#include <iostream>
+#include <sstream>
+#ifndef TOURGUIADO_H
+#define TOURGUIADO_H
+
+class TourGuiado {
+    private:
+        std::string agencia;
+        std::set<std::string> lugaresVisitados;
+    public:
+        TourGuiado();
+        TourGuiado(std::string, std::set<std::string>),
+
+        std::string getAgencia();
+        std::set<std::string> getLugaresVisitados();
+        
+        //¿podria querer modificar la empresa responsable y el itinerario de lugares a visitar?
+        void setAgencia(std::string);
+        void setLugaresVisitados(std::set<std::string>);
+
+        float calcularCosto();
+        ~TourGuiado();
+}
+
+#endif 
