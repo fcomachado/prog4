a0fc40895f2cd5c6eb4ced073914f8d7d661aa79
fcomachado <fmachadotrinidad@gmail.com>
2026-03-09 18:29:31 -0300

    experiencia.h v2

--- include/Experiencia.h
+++ include/Experiencia.h
@@ -3,21 +3,29 @@
 #include <sstream>
 #include <DTFecha.h>
 #include <DTExpe.h>
-#ifndef EXPERIENCIA_h
-#define EXPERIENCIA_h
+#ifndef EXPERIENCIA_H
+#define EXPERIENCIA_H
 
 class Experiencia {
     private:
-       string codigoReserva;
-       string descripcion;
+       std::string codigoReserva;
+       std::string descripcion;
        int precioBase;
        DTFecha fecha;
 
     public:
+        Experiencia();
+        Experiencia(std::string, std::string, int, DTFecha);
+
+        std::string getCodigoReserva();
+        std::string getDescripcion();
+        int getPrecioBase();
+        DTFecha getFecha();
 
         DTExpe getDT();
         float calcularCosto();
        
+        ~Experiencia();
 }
 
 #endif
