f467d842c53c63cc23a9a3596dbddf10590f2792
IsmaSchiavone <ismael.schiavone.meny@gmail.com>
2026-03-11 10:59:55 -0300

    Update Alojamiento.h

--- include/Alojamiento.h
+++ include/Alojamiento.h
@@ -1,1 +1,20 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#ifndef ALOJAMIENTO_H
+#define ALOJAMIENTO_H
 
+class Alojamiento: public Experiencia{
+    private:
+        std::string hotel;
+        TipoRegimen regimen;
+        int cantNoches;
+    public:
+        calcularCosto();
+
+        TipoRegimen getTipoRegimen();
+        int getcantNoches();
+        int getprecioBase();
+};
+
+#endif
