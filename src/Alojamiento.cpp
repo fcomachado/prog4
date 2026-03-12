bf93c55797102b0e1ccf2ac164823e5d7ca1a85e
IsmaSchiavone <ismael.schiavone.meny@gmail.com>
2026-03-12 16:05:01 -0300

    Cambios en Alojamiento.cpp

--- src/Alojamiento.cpp
+++ src/Alojamiento.cpp
@@ -4,7 +4,13 @@
 #include <Experiencia.h>
 #include <Alojamiento.h>
 
+Alojamiento::Alojamiento() {}
 
+Alojamiento::Alojamiento(std::string hotel, TipoRegimen regimen, int cantNoches){
+  this->hotel = hotel;
+  this->regimen = regimen;
+  this->cantNoches = cantNoches;
+}
 
 float Alojamiento::calcularCosto(){
   if (TipoRegimen == AllInculsive){
@@ -12,13 +18,28 @@ float Alojamiento::calcularCosto(){
   } else{ 
     calcularCosto = precioBase*cantNoches;
   }
-}
+};
 
 TipoRegimen Alojamiento::getTipoRegimen(){
   return this->regimen;
-}
+};
 
 int Alojamiento::getcantNoches(){
   return this->cantNoches;
-}
+};
+
+void setHotel(std::string hotel){
+  this->hotel = hotel;
+};
+
+void setRegimen(TipoRegimen regimen){
+  this->regimen = regimen;
+};
+
+void setCantNoches(int cantNoches){
+  this->cantNoches = cantNoches;
+};
+
+Alojamiento::~Alojamiento(){}      
+
 
